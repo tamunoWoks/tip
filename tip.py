74bddedde9242a0a144ec005606b2fbfa63f3114
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-21 18:35:41 -0600

    cleaned up code

--- tip.py
+++ tip.py
@@ -1,16 +1,19 @@
 def main():
     dollars = dollars_to_float(input("How much was the meal? "))
-    percent = percent_to_float(input('What percentage would you like to tip? '))
+    percent = percent_to_float(input("What percentage would you like to tip? "))
     tip = dollars * percent
-    print(f'Leave ${tip:.2f}')
-      
+    print(f"Leave ${tip:.2f}")
+
+
 def dollars_to_float(d):
-    amount = float(d.replace('$', ''))
+    amount = float(d.replace("$", ""))
     return round(amount, 2)
 
-def percent_to_float(p):
-    percentage = float(p.replace('%', ''))
-    return percentage/100
 
-if __name__ == '__main__':
+def percent_to_float(p):
+    percentage = float(p.replace("%", ""))
+    return percentage / 100
+
+
+if __name__ == "__main__":
     main()
