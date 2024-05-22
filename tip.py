e59cfdd6d4f7e78a0aa78eacedad35acab043a4b
tamunoWoks <tamunosakiwokoma86@gmail.com>
2024-05-21 18:30:13 -0600

    create function that converts a value to a percentage fraction

--- tip.py
+++ tip.py
@@ -1,3 +1,7 @@
 def dollars_to_float(d):
     amount = float(d.replace('$', ''))
     return round(amount, 2)
+
+def percent_to_float(p):
+    percentage = float(p.replace('%', ''))
+    return percentage/100
